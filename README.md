78da94069cce0c3c1ab96f9fb9fb1368fa88c48a
misato86 <63838@gymnasium.ax>
2025-10-03 13:56:23 +0300

    Uppstart

---
+++ README.md
@@ -0,0 +1,2 @@
+# sten sax påse
+Detta är en liten vue applikation för att spela sten sax påse.
---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>Sten, sax, påse!</h1>
+
+  </header>
+
+  <main>
+    <div class="knapprad">
+      <button>Sten</button>
+      <button>Sax</button>
+      <button>Påse</button>
+    </div>
+    <div class="resultat">
+      <p id="resultat">Du vann!</p>
+    </div>
+    <div class="score">
+      <p>
+        <span id="spelare">0</span> - <span id="dator">0</span>
+      </p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  header {
+    text-align: center;
+    margin-bottom: 1,2em;
+  }
+  button {
+     padding: .6em 1.2em;
+      font-size: 1.2em;
+      background-color: #f0f0f0;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      cursor: pointer;
+  }
+  .knapprad {
+    display: flex;
+    justify-content: center;
+    gap: .6em;
+  }
+  .resultat {
+    text-align: center;
+    font-size: 1.2em;
+    margin: 1,2em 0;
+  }
+  .score {
+    text-align: center;
+    font-size: 1.2em;
+
+  }
+</style>
